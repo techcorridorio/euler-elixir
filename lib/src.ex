14971141fc0d8f1aab8bec0a9aaedf4f1c68dd98
Benjamin Oakes <boakes@liaisonedu.com>
2016-12-07 19:02:40 -0600

    Euler problem no. 1 /w @chrisortman and @rileylark

--- lib/src.ex
+++ lib/src.ex
@@ -1,7 +1,14 @@
 defmodule Src do
-  def multiples do
-    IO.puts("got in")
+  def multiples(n) do
+    range = 1..(n - 1)
+    subset = Enum.filter(range, &is_multiple/1)
+    Enum.sum(subset)
+  end
+
+  defp is_multiple(x) do
+    rem(x, 3) == 0 || rem(x, 5) == 0
   end
 end
 
-Src.multiples
+IO.puts(Src.multiples(10))
+IO.puts(Src.multiples(1000))
