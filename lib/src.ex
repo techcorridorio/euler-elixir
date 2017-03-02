c0dd0cf86c53407fc845482fdb57296ca9afe958
Benjamin Oakes <hello@benjaminoakes.com>
2017-03-01 20:02:04 -0600

    Add prime? and factors

--- lib/src.ex
+++ lib/src.ex
@@ -33,4 +33,33 @@ defmodule Src do
     next_fib = first_fib + second_fib
     fib(remaining - 1, second_fib, next_fib)
   end
+
+  @doc ~S"""
+    iex> Src.prime?(2)
+    true
+
+    iex> Src.prime?(3)
+    true
+
+    iex> Src.prime?(4)
+    false
+  """
+  def prime?(x) do
+    #length(factors(x)) == 2
+    case factors(x) do
+      [1,^x] -> true
+      _ -> false
+    end
+  end
+
+  @doc ~S"""
+    iex> Src.factors(2)
+    [1,2]
+
+    iex> Src.factors(4)
+    [1,2,4]
+  """
+  def factors(x) do
+    (1..x) |> Enum.filter(&( rem(x,&1) == 0))
+  end
 end
